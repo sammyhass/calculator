caf3ca983eceaad05d749ae86376e13083079ef9
Sammy Hass <sammyhass01@gmail.com>
2020-08-30 15:03:08 +0100

    Updated styling for 0 button

--- src/components/NumButton/index.js
+++ src/components/NumButton/index.js
@@ -4,7 +4,11 @@ import styles from './NumButton.module.css';
 
 const NumButton = ({ num, click }) => {
 	return (
-		<button onClick={click} className={styles.NumButton}>
+		<button
+			onClick={click}
+			className={styles.NumButton}
+			style={num === 0 ? { gridColumnEnd: 'span 3' } : null}
+		>
 			{num}
 		</button>
 	);
