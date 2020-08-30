9d2353af35d053c8288fa01fb6c704ebb904f68b
Sammy Hass <sammyhass01@gmail.com>
2020-08-30 14:53:11 +0100

    cleanup and updated title

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import ResultWindow from './ResultWindow';
 import ButtonContainer from './ButtonsContainer';
